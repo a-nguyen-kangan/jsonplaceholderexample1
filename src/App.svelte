48577127f7a7c45beaf1d7520a335fd9d580eaf5
a-nguyen-kangan <109706990+a-nguyen-kangan@users.noreply.github.com>
2023-07-31 12:56:46 +1000

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+
+let postNum;
+
+async function getPostItem(id) {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts/'+id);
+  const post = await res.json();
+  
+  console.log(post);
+
+  return post;
+    
+}
+
+function handleClick() {
+  promise = getPostItem(postNum);
+}
+
+let promise = getPostItem();
+
+</script>
+
+<input type='number' bind:value={postNum}/>
+<button on:click={handleClick}>Get Post</button>
+
+<main>
+
+{#await promise}
+<p>Waiting...</p>
+
+{:then post}
+
+<h2>{post.title}</h2>
+<p>{post.body}<p>
+
+{/await}
+
+
+</main>
+
+<style>
+
+</style>
